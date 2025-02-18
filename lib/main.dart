7a259002c216472e1b4b580d653fa782242076cb
abir3262 <hasan15-5459@diu.edu.bd>
2025-02-18 10:57:36 +0600

    icon overflow

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,46 @@ class LabClass05 extends StatelessWidget {
         ),
         body: Container(
           height: double.infinity,
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
               Icon(
                 Icons.star,
                 size: 50,
