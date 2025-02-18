a44979b3b89cdfe3acd5b9253b70e67c3f7fa62d
abir3262 <hasan15-5459@diu.edu.bd>
2025-02-18 10:47:53 +0600

    icon used in middle

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,34 @@ class LabClass05 extends StatelessWidget {
           backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Row(
-          children: [
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star,
-             size: 50, 
-            ),
-            Icon(
-             Icons.star_outline,
-             size: 50, 
-            ),
-          ],
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star,
+                size: 50,
+              ),
+              Icon(
+                Icons.star_outline,
+                size: 50,
+              ),
+            ],
+          ),
         ),
       ),
     );
